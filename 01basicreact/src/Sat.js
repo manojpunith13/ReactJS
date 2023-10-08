4f7a756a923f32c2ea66e3df559e14fda50da10a
manojpunith13 <manojpunith13@gmail.com>
2023-10-08 14:15:59 +0530

    react and jsx

---
+++ 01basicreact/src/Sat.js
@@ -0,0 +1,10 @@
+function Sat(){
+    return(
+        <> <img  src="https://i.imgur.com/lICfvbD.jpg" alt="Like me"/>
+     <div className="like" style={{backgroundColor:'orange'}}>Crazy saturday</div>
+      
+        </>
+    )
+}
+
+export default Sat;
